36ef26282545ac5bee96e457f1b849bf4929e2fe
Alejandro <acuellarh@gmail.com>
2020-02-19 21:32:12 -0500

    Create and index view for Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,24 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all.order(created_at: :desc)
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.create(movie_params)
+    redirect_to movies_path
   end
 
   def destroy
   end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description, :url, :date)
+  end
+  
 end
