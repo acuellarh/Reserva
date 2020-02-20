e8dae0ea1411458147da298c32ab18d7c64fea1b
Alejandro <acuellarh@gmail.com>
2020-02-19 22:41:19 -0500

    Reservation view and modal create - modal doesnt work

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,29 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.create(reservation_params)
+    @movie = Movie.find(params[:id])
+    @reservation.movies <<  @movie
+    redirect_to reservations_path
   end
 
   def destroy
   end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:name, :cedula, :celular, :email)
+  end
+  
 end
